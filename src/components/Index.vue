a2371843103657618d7493f22a5e5431d578d03f
Tyler Smith <tsmith18256@gmail.com>
2017-09-16 21:59:38 -0700

    Add home page with header

---
+++ src/components/Index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <header class="header">
+      <h1 class="title">Tyler Smith:</h1>
+      <h1 class="title">Full Stack</h1>
+      <h1 class="title">Developer</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+  .header {
+    display: flex;
+
+    width: 100vw;
+    height: 15rem;
+
+    padding: 0 2rem;
+
+    flex-direction: column;
+    justify-content: center;
+
+    background-color: var(--bg-dark-color);
+  }
+
+  .title {
+    /* TODO: Add Source Sans Pro as a static resource */
+    font: 2.5rem 'Source Code Pro';
+    letter-spacing: -0.05rem;
+
+    color: var(--text-light-color);
+  }
+
+  /* TODO: add PostCSS Nested plugin */
+  .title:last-of-type::after {
+    content: '';
+    display: inline-block;
+
+    width: 1px;
+    height: 1em;
+
+    margin: 0.15em 0 0 0.1em;
+
+    vertical-align: top;
+
+    background-color: #AAA;
+
+    animation: 1s step-end caret-blink infinite;
+  }
+
+  @keyframes caret-blink {
+    0% {
+      opacity: 1.0;
+    }
+
+    50% {
+      opacity: 0.0;
+    }
+
+    100% {
+      opacity: 1.0;
+    }
+  }
+</style>
